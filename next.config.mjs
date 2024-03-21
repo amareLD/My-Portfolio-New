b583b073358f6b707b84536b2e6705285af26c6e
amareLD <80774058+amareLD@users.noreply.github.com>
2024-03-21 22:58:09 +0530

    after-build

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output:'export',
+  output: 'export',
   images: {
+    loader: 'custom',
+    loaderFile: './loader.js',
     domains: ['images.pexels.com', 'www.instagram.com'],
   },
 };
