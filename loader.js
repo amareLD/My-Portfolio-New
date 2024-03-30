9a35fdd6c0b01d3cdb76d80516f09e0b5a57c62b
amareLD <80774058+amareLD@users.noreply.github.com>
2024-03-30 11:44:20 +0530

    cancelled-image-optimization

--- loader.js
+++ loader.js
@@ -1,6 +1,10 @@
 'use client';
 
-export default function myImageLoader({ src, width, quality }) {
-  if (src.startsWith('https://image.pexels.com')) return src;
-  return `https://nextjsportfolio.com/${src}?w=${width}&q=${quality}||75`;
+// export default function myImageLoader({ src, width, quality }) {
+//   if (src.startsWith('https://image.pexels.com')) return src;
+//   return `https://nextjsportfolio.com/${src}?w=${width}&q=${quality}||75`;
+// }
+
+export default function myImageLoader({ src }) {
+  return src;
 }
